ca568d547c9d73bf478802b8d36841be2b994ac4
Benedikt <s6beotto@uni-bonn.de>
2020-01-15 10:26:42 +0100

    Added timing.py to check performance

---
+++ src/timing.py
@@ -0,0 +1,25 @@
+from tools import Potential, Action, Metropolis
+from time import time
+
+# number of lattice positions
+N = 1000
+
+# parameters
+mass = 0.01
+mu = 10
+lambda_ = 0
+initval = 0
+
+tau = 0.1
+
+p = Potential(mu, lambda_)
+a = Action(tau, mass, p)
+m = Metropolis(N, a, borders = [-10, 10], initval=initval)
+
+
+N = 100
+t1 = time()
+for i in range(N):
+    list(m)
+t2 = time()
+print((t2 - t1) / N)
