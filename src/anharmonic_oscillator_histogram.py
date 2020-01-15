e57a00b658389cd7fd9475ccacd55ac39ac56277
Benedikt <s6beotto@uni-bonn.de>
2020-01-15 10:52:48 +0100

    Added seperate histogram files

---
+++ src/anharmonic_oscillator_histogram.py
@@ -0,0 +1,33 @@
+from tools import Potential, Action, Metropolis
+from matplotlib import pyplot as plt
+import numpy as np
+
+# number of lattice positions
+N = 100000
+
+# parameters
+mass = 0.01
+mu = -10
+lambda_ = -0.02
+initval = -5
+
+step = 0.1
+
+tau = 0.1
+
+p = Potential(mu, lambda_)
+
+a = Action(tau, mass, p)
+
+m = Metropolis(N, a, borders = [-10, 10], initval=initval)
+
+vals = list(m)
+
+bins = np.arange(*m.borders, step=step)
+
+values = np.bincount(np.digitize(vals, bins), minlength=len(bins))
+
+plt.figure()
+plt.errorbar(bins, values, yerr = np.sqrt(values), fmt='.')
+plt.xlim(*m.borders)
+plt.show()
