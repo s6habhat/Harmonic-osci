41b2f071120535086db36b749eba604705775d15
s6beotto <s6beotto@uni-bonn.de>
2020-01-19 10:26:32 +0100

    added file exists check

--- src/anharmonic_oscillator_lambda_parameter.py
+++ src/anharmonic_oscillator_lambda_parameter.py
@@ -6,13 +6,13 @@ from pathlib import Path
 import csv
 
 # number of lattice positions
-N = 1000
+N = 100000
 
 # parameters
 mass = 0.01
 mu = -10
 
-min_distance, max_distance, step_distance = 0.1, 16, 0.1
+min_distance, max_distance, step_distance = 0, 16, 0.05
 
 start, stop, step = -10, 10, 0.1
 
@@ -20,9 +20,23 @@ tau = 0.1
 
 hbar = 1
 
-distances = np.arange(min_distance, max_distance, step_distance)
+distances = np.arange(min_distance + step_distance, max_distance + step_distance, step_distance)
 bins = np.arange(start, stop + step, step=step)
-print(bins)
+
+root_path = getRootDirectory()
+dir_ = root_path / 'data' / 'anharmonic_oscillator_lambda_parameter'
+dir_.mkdir(exist_ok=True)
+file_ = dir_ / ('d%0.2f-%0.2f-%0.2fs%0.2f-%0.2f-%0.2f-N%d.csv' % (min_distance, max_distance, step_distance, start, stop, step, N))
+if file_.exists():
+	i = input('Overwrite file "%s"? ' %file_.relative_to(root_path)).lower()
+	if i == 'y':
+		pass
+	else:
+		num = 1
+		while file_.exists():
+			file_ = dir_ / ('d%0.2f-%0.2f-%0.2fs%0.2f-%0.2f-%0.2f-N%d_%d.csv' % (min_distance, max_distance, step_distance, start, stop, step, N, num))
+			num += 1
+
 
 def calculatePositionDistribution(distance):
 	print("calculating for distance=%0.2f" % distance)
@@ -36,12 +50,6 @@ def calculatePositionDistribution(distance):
 
 p = Pool()
 results = p.map(calculatePositionDistribution, distances)
-
-root_path = getRootDirectory()
-dir_ = root_path / 'data' / 'anharmonic_oscillator_lambda_parameter'
-dir_.mkdir(exist_ok=True)
-
-file_ = dir_ / ('d%0.2f-%0.2f-%0.2fs%0.2f-%0.2f-%0.2f-N%d.csv' % (min_distance, max_distance, step_distance, start, stop, step, N))
 
 with file_.open('w', newline='') as file:
 	writer = csv.writer(file)
