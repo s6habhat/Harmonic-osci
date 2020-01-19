c3de386ddc20d4996a51e23b6cdc020c07979197
s6beotto <s6beotto@uni-bonn.de>
2020-01-19 11:02:15 +0100

    Added file existing check, increased resolution in filename

--- src/anharmonic_oscillator_classical_limit.py
+++ src/anharmonic_oscillator_classical_limit.py
@@ -19,6 +19,20 @@ tau = 0.1
 
 hbar_start, hbar_stop, hbar_step = 0.0, 2.0, 0.005
 
+root_path = getRootDirectory()
+dir_ = root_path / 'data' / 'anharmonic_oscillator_classical_limit'
+dir_.mkdir(exist_ok=True)
+file_ = dir_ / ('h%0.2f-%0.2f-%0.4f_%0.2f-%0.2f-%0.2f-N%d.csv' % (hbar_start, hbar_stop, hbar_step, start, stop, step, N))
+if file_.exists():
+	i = input('Overwrite file "%s"? ' %file_.relative_to(root_path)).lower()
+	if i == 'y':
+		pass
+	else:
+		num = 1
+		while file_.exists():
+			file_ = dir_ / ('h%0.2f-%0.2f-%0.4f_%0.2f-%0.2f-%0.2f-N%d_%d.csv' % (hbar_start, hbar_stop, hbar_step, start, stop, step, N, num))
+			num += 1
+
 hbars = np.arange(hbar_start + hbar_step, hbar_stop + hbar_step, hbar_step)
 bins = np.arange(start, stop + step, step=step)
 
@@ -33,12 +47,6 @@ def calculatePositionDistribution(hbar):
 
 p = Pool()
 results = p.map(calculatePositionDistribution, hbars)
-
-root_path = getRootDirectory()
-dir_ = root_path / 'data' / 'anharmonic_oscillator_classical_limit'
-dir_.mkdir(exist_ok=True)
-
-file_ = dir_ / ('h%0.2f-%0.2f-%0.4f_%0.2f-%0.2f-%0.2f-N%d.csv' % (hbar_start, hbar_stop, hbar_step, start, stop, step, N))
 
 with file_.open('w', newline='') as file:
 	writer = csv.writer(file)
