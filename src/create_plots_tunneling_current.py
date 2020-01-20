70d13aa9493591005aac289b0024a30e772efa62
Benedikt <s6beotto@uni-bonn.de>
2020-01-20 11:01:50 +0100

    Renamed to src/create_plots_tunneling_current.py, added pattern matching

--- src/create_plots_tunneling_current.py
+++ src/create_plots_tunneling_current.py
@@ -1,8 +1,7 @@
 from matplotlib import pyplot as plt
-from matplotlib.colors import LogNorm
-import sys
 from tools import getRootDirectory
 import csv
+import sys
 
 if len(sys.argv) >= 2:
 	dirs = sys.argv[1:]
@@ -19,39 +18,25 @@ for subdir_pattern in dirs:
 		if not filename.exists() or filename.is_dir():
 			continue
 
+		distances = []
+		tunneling_rates = []
+
 		with filename.open('r') as csvfile:
 			reader = csv.reader(csvfile)
-			distances = []
-			datas = []
 			for i, row in enumerate(reader):
+				#print(row)
 				if i == 0:
-					header_min = [float(v) for v in row[1:]]
-				elif i == 1:
-					header_max = [float(v) for v in row[1:]]
+					pass
 				else:
 					distance = float(row[0])
-					data = [int(v) for v in row[1:]]
+					tunneling_rate = int(row[1])
 					distances.append(distance)
-					datas.append(data)
+					tunneling_rates.append(tunneling_rate)
 
-
-		fig, ax = plt.subplots(figsize=(6,6))
-		cs = ax.imshow(datas, extent=[min(header_min), max(header_max), max(distances), min(distances)], norm=LogNorm())
-
-		cbar = fig.colorbar(cs)
-		cbar.ax.minorticks_off()
-
-		plt.plot([+d / 2 for d in distances], distances, color='black', label='Classical Minima')
-		plt.plot([-d / 2 for d in distances], distances, color='black')
-
-		x0,x1 = ax.get_xlim()
-		y0,y1 = ax.get_ylim()
-		ax.set_aspect(abs(x1-x0)/abs(y1-y0))
-
-		plt.xlabel('position')
-		plt.ylabel('minima distance')
-
-		plt.legend()
+		plt.figure()
+		plt.errorbar(distances, tunneling_rates, fmt='.')
+		plt.xlabel('Distance')
+		plt.ylabel('Tunneling rate')
 
 		out_filename = root_path / 'imgs' / relative_path
 		out_filename.parent.mkdir(exist_ok=True)
