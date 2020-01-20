78b7e66cd1f92eb04dc34bfad7d738e82f9f3fb4
Benedikt <s6beotto@uni-bonn.de>
2020-01-20 10:20:57 +0100

    Added glob pattern matching, fixed tabs, added debug output, fixed not accept directories

--- src/create_plots_track.py
+++ src/create_plots_track.py
@@ -4,40 +4,43 @@ import csv
 import sys
 
 if len(sys.argv) >= 2:
-    dirs = sys.argv[1:]
+	dirs = sys.argv[1:]
 else:
-    exit(-1)
+	exit(-1)
 
 root_path = getRootDirectory()
 
-for subdir in dirs:
-    filename = root_path / subdir
-    relative_path = filename.relative_to(root_path / 'data')
+for subdir_pattern in dirs:
+	for subdir in root_path.glob(subdir_pattern):
+		filename = root_path / subdir
+		relative_path = filename.relative_to(root_path / 'data')
 
-    if not filename.exists():
-        continue
+		if not filename.exists() or filename.is_dir():
+			continue
 
-    nums = []
-    distances = []
+		nums = []
+		distances = []
 
-    with filename.open('r') as csvfile:
-        reader = csv.reader(csvfile)
-        for i, row in enumerate(reader):
-            if i == 0:
-                pass
-            else:
-                num = int(row[0])
-                distance = float(row[1])
-                nums.append(num)
-                distances.append(distance)
+		with filename.open('r') as csvfile:
+			reader = csv.reader(csvfile)
+			for i, row in enumerate(reader):
+				if i == 0:
+					pass
+				else:
+					num = int(row[0])
+					distance = float(row[1])
+					nums.append(num)
+					distances.append(distance)
 
-    plt.figure()
-    plt.errorbar(distances, nums)
-    plt.xlabel('Distance')
-    plt.ylabel('Number')
+		print("\n\nComputing file %s:" %relative_path)
 
-    out_filename = root_path / 'imgs' / relative_path
-    out_filename.parent.mkdir(exist_ok=True)
+		plt.figure()
+		plt.errorbar(distances, nums)
+		plt.xlabel('Distance')
+		plt.ylabel('Number')
 
-    plt.savefig(out_filename.with_suffix(".png"))
-    plt.savefig(out_filename.with_suffix(".pdf"))
+		out_filename = root_path / 'imgs' / relative_path
+		out_filename.parent.mkdir(exist_ok=True)
+
+		plt.savefig(out_filename.with_suffix(".png"))
+		plt.savefig(out_filename.with_suffix(".pdf"))
