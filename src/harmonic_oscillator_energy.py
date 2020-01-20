b438ad9d03ea9e000dd3f433902eb57d85594fea
Benedikt <s6beotto@uni-bonn.de>
2020-01-20 10:17:29 +0100

    Added glob pattern matching harmonic_oscillator_energy.py, fixed variable names

--- src/harmonic_oscillator_energy.py
+++ src/harmonic_oscillator_energy.py
@@ -3,6 +3,7 @@ from matplotlib.colors import LogNorm
 import sys
 from tools import getRootDirectory
 import csv
+import numpy as np
 
 if len(sys.argv) >= 2:
 	dirs = sys.argv[1:]
@@ -11,23 +12,23 @@ else:
 
 root_path = getRootDirectory()
 
-for subdir in dirs:
-	filename = root_path / subdir
-	relative_path = filename.relative_to(root_path / 'data')
+for subdir_pattern in dirs:
+	for subdir in root_path.glob(subdir_pattern):
+		filename = root_path / subdir
+		relative_path = filename.relative_to(root_path / 'data')
 
-	if not filename.exists():
-		continue
-	with filename.open('r') as csvfile:
-		reader = csv.reader(csvfile)
-		distances = []
-		datas = []
-		for i, row in enumerate(reader):
-			if i == 0:
-				header_min = [float(v) for v in row[1:]]
-			elif i == 1:
-				header_max = [float(v) for v in row[1:]]
-			else:
-				distance = float(row[0])
-				data = [int(v) for v in row[1:]]
-				distances.append(distance)
-				datas.append(data)
+		if not filename.exists():
+			continue
+		with filename.open('r') as csvfile:
+			reader = csv.reader(csvfile)
+			positions = []
+			for i, row in enumerate(reader):
+				if i == 0:
+					header_min = row[:]
+				else:
+					num = int(row[0])
+					position = float(row[1])
+					positions.append(position)
+		print("\n\nComputing file %s:" %relative_path)
+		positions = np.array(positions)
+		print(positions)
