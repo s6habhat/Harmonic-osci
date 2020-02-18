380686d2e0a834869bdfa4e4e5c71e55f8c3b8b3
Benedikt <s6beotto@uni-bonn.de>
2020-02-18 17:49:46 +0100

    Added support for new time data files

--- src/create_plots_track.py
+++ src/create_plots_track.py
@@ -1,46 +1,56 @@
 from matplotlib import pyplot as plt
 from tools import getRootDirectory
 import csv
-import sys
+import numpy as np
 
-if len(sys.argv) >= 2:
-	dirs = sys.argv[1:]
-else:
-	exit(-1)
+import argparse
+from glob import glob
+
+import itertools
+flatten = itertools.chain.from_iterable
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument("filenames", nargs='*')
+parser.add_argument('-i', "--iterations", nargs='+')
+args = parser.parse_args()
+
+iterations_used = args.iterations
+
+filenames = list(flatten([glob(arg) for arg in args.filenames]))
 
 root_path = getRootDirectory()
 
-for subdir_pattern in dirs:
-	for subdir in root_path.glob(subdir_pattern):
-		filename = root_path / subdir
-		relative_path = filename.relative_to(root_path / 'data')
+for file in filenames:
+	full_path = (root_path / file)
+	if not full_path.exists() or full_path.is_dir():
+		continue
 
-		if not filename.exists() or filename.is_dir():
-			continue
+	relative_path = full_path.relative_to(root_path / 'data')
 
-		nums = []
-		distances = []
+	print("\nComputing file %s ..." %relative_path)
 
-		with filename.open('r') as csvfile:
-			reader = csv.reader(csvfile)
-			for i, row in enumerate(reader):
-				if i == 0:
-					pass
-				else:
-					num = int(row[0])
-					distance = float(row[1])
-					nums.append(num)
-					distances.append(distance)
+	data = {}
 
-		print("\n\nComputing file %s:" %relative_path)
-
-		plt.figure()
-		plt.errorbar(distances, nums)
+	with full_path.open('r') as csvfile:
+		reader = csv.reader(csvfile)
+		for i, row in enumerate(reader):
+			if i == 0:
+				numbers = np.array([int(r) for r in row[1:]])
+				pass
+			else:
+				num = int(row[0])
+				positions = np.array([float(r) for r in row[1:]])
+				data[num] = positions
+	plt.figure()
+	for iteration in iterations_used:
+		iteration = int(iteration)
+		plt.errorbar(data[iteration], numbers, label='path after %d iteration' %(iteration + 1))
 		plt.xlabel('Position')
 		plt.ylabel('Number')
+	plt.legend()
 
-		out_filename = root_path / 'imgs' / relative_path
-		out_filename.parent.mkdir(exist_ok=True)
+	out_filename = root_path / 'imgs' / relative_path
+	out_filename.parent.mkdir(exist_ok=True)
 
-		plt.savefig(out_filename.with_suffix(".png"))
-		plt.savefig(out_filename.with_suffix(".pdf"))
+	plt.savefig(out_filename.with_suffix(".png"))
+	plt.savefig(out_filename.with_suffix(".pdf"))
