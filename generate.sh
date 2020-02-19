7b1aa06e4cb44adf79047266565f4e4ffd2f6a5b
s6beotto <s6beotto@uni-bonn.de>
2020-02-19 22:42:41 +0100

    Added help function, added long cli arguments

--- generate.sh
+++ generate.sh
@@ -2,15 +2,27 @@
 
 PYTHON="/usr/bin/python3"
 
+if [ "${1}" = "-h" ] || [ "${1}" = "--help" ]; then
+    echo "usage: ${0} [-h] [-c] [-f]"
+	echo
+	echo "Create and plot the data"
+	echo
+	echo "optional arguments:"
+	echo "  -h, --help		show this help message and exit"
+	echo "  -c, --clean		clean data and image directories"
+	echo "  -f, --force		force overwrite of data files"
+    exit 1
+fi
+
 # cleanup
-if [ "${1}" = "-c" ]; then
+if [ "${1}" = "-c" ] || [ "${1}" = "--clean" ]; then
     rm -r data/* 2> /dev/null
     rm -r imgs/* 2> /dev/null
     exit 1
 fi
 
 # evolution of path from 5 to 0
-if [ ! -f data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv ] || [ "${1}" = "-f" ]; then
+if [ ! -f data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
     echo "Generating file data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv"
     $PYTHON src/harmonic_oscillator.py -m 0.25 -init 5 -i 100 -N 100
 else
@@ -23,7 +35,7 @@ $PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N100i100init5.0
 echo
 
 # evolution of path from 5 to 0
-if [ ! -f data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv ] || [ "${1}" = "-f" ]; then
+if [ ! -f data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
     echo "Generating file data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv"
     $PYTHON src/harmonic_oscillator.py -m 0.25 -init 5 -i 100 -N 1000
 else
@@ -36,7 +48,7 @@ $PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N1000i100init5.
 echo
 
 # evolution of path from 5 to 0
-if [ ! -f data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv ] || [ "${1}" = "-f" ]; then
+if [ ! -f data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
     echo "Generating file data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv"
     $PYTHON src/harmonic_oscillator.py -m 0.25 -init 5 -i 100 -N 10000
 else
@@ -49,7 +61,7 @@ $PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N10000i100init5
 echo
 
 # evolution of path from 5 to 0 better
-if [ ! -f data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv ] || [ "${1}" = "-f" ]; then
+if [ ! -f data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
     echo "Generating file data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv"
     $PYTHON src/harmonic_oscillator.py -m 0.25 -init 5 -i 1000 -N 1000
 else
