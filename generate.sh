da39fa4c0c73e0d78d9c208a8c2c740d0cf94bb6
Benedikt <s6beotto@uni-bonn.de>
2020-02-26 12:52:50 +0100

    Made generate.sh more compact

--- generate.sh
+++ generate.sh
@@ -22,164 +22,180 @@ if [ "${1}" = "-c" ] || [ "${1}" = "--clean" ]; then
 fi
 
 # harmonic oscillator
+if [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
+    force=true
+else
+    force=false
+fi
 
 # evolution of path from 5 to 0
-if [ ! -f data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv"
+FILE=data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/harmonic_oscillator.py -m 0.25 -init 5 -ir 1 -i 100 -N 100
 else
-    echo "Not generating file data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_track.py data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv -i 1 10 20 40 80 100
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv -i 1 10 20 40 80 100 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N100i100init5.0000m0.2500.csv -i 10 20 40 80 100 -f
+$PYTHON src/create_plots_track.py $FILE -i 1 10 20 40 80 100
+$PYTHON src/create_plots_gauss.py $FILE -i 1 10 20 40 80 100 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 10 20 40 80 100 -f
 
 echo
 
 # evolution of path from 5 to 0
-if [ ! -f data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv"
+FILE=data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/harmonic_oscillator.py -m 0.25 -init 5 -ir 1 -i 100 -N 1000
 else
-    echo "Not generating file data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_track.py data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv -i 1 10 20 40 80 100
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv -i 1 10 20 40 80 100 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N1000i100init5.0000m0.2500.csv -i 10 20 40 80 100 -f
+$PYTHON src/create_plots_track.py $FILE -i 1 10 20 40 80 100
+$PYTHON src/create_plots_gauss.py $FILE -i 1 10 20 40 80 100 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 10 20 40 80 100 -f
 
 echo
 
 # evolution of path from 5 to 0
-if [ ! -f data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv"
+FILE=data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/harmonic_oscillator.py -m 0.25 -init 5 -ir 1 -i 100 -N 10000
 else
-    echo "Not generating file data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_track.py data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv -i 1 10 20 40 80 100
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv -i 1 10 20 40 80 100 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv -i 10 20 40 80 100 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv -i 1 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv -i 10 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv -i 20 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv -i 40 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv -i 80 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N10000i100init5.0000m0.2500.csv -i 100 -f
+$PYTHON src/create_plots_track.py $FILE -i 1 10 20 40 80 100
+$PYTHON src/create_plots_gauss.py $FILE -i 1 10 20 40 80 100 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 10 20 40 80 100 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 1 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 10 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 20 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 40 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 80 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 100 -f
 
 echo
 
 # evolution of path from 5 to 0 better
-if [ ! -f data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv"
+FILE=data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/harmonic_oscillator.py -m 0.25 -init 5 -ir 1 -i 1000 -N 1000
 else
-    echo "Not generating file data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_track.py data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv -i 1 10 20 40 80 100 1000
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv -i 1 10 20 40 80 100 1000 -f
-$PYTHON src/create_plots_gauss.py data/harmonic_oscillator_track/N1000i1000init5.0000m0.2500.csv -i 10 20 40 80 100 1000 -f
+$PYTHON src/create_plots_track.py $FILE -i 1 10 20 40 80 100 1000
+$PYTHON src/create_plots_gauss.py $FILE -i 1 10 20 40 80 100 1000 -f
+$PYTHON src/create_plots_gauss.py $FILE -i 10 20 40 80 100 1000 -f
 
 echo
 
 # evolution of a step state
-if [ ! -f data/harmonic_oscillator_track/N100i100initstepm0.2500step.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/harmonic_oscillator_track/N100i100initstepm0.2500step.csv"
+FILE=data/harmonic_oscillator_track/N100i100initstepm0.2500step.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/harmonic_oscillator.py -m 0.25 -i 100 -N 100 --step
 else
-    echo "Not generating file data/harmonic_oscillator_track/N100i100initstepm0.2500step.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_track.py data/harmonic_oscillator_track/N100i100initstepm0.2500step.csv -i 1 10 20 40 80 100
-$PYTHON src/create_plots_track_shifted.py data/harmonic_oscillator_track/N100i100initstepm0.2500step.csv -i 1 10 20 40 80 100
-$PYTHON src/create_plots_track_shifted.py data/harmonic_oscillator_track/N100i100initstepm0.2500step.csv -i 1 5 10 15 20 25
-$PYTHON src/create_plots_track_shifted.py data/harmonic_oscillator_track/N100i100initstepm0.2500step.csv -i 1 2 3 4 5 6 7 8 9 10
-$PYTHON src/create_plots_track_shifted.py data/harmonic_oscillator_track/N100i100initstepm0.2500step.csv -i 1 3 5 7 9 11 13 15
+$PYTHON src/create_plots_track.py $FILE -i 1 10 20 40 80 100
+$PYTHON src/create_plots_track_shifted.py $FILE -i 1 10 20 40 80 100
+$PYTHON src/create_plots_track_shifted.py $FILE -i 1 5 10 15 20 25
+$PYTHON src/create_plots_track_shifted.py $FILE -i 1 2 3 4 5 6 7 8 9 10
+$PYTHON src/create_plots_track_shifted.py $FILE -i 1 3 5 7 9 11 13 15
 
 echo
 
 
 # anharmonic oscillator
-
-if [ ! -f data/anharmonic_oscillator_track/N100i1000init0.0m0.2500l-0.0200d10.0000.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/anharmonic_oscillator_track/N100i1000init0.0m0.2500l-0.0200d10.0000.csv"
+FILE=data/anharmonic_oscillator_track/N100i1000init0.0m0.2500l-0.0200d10.0000.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/anharmonic_oscillator.py -m 0.25 -init 0 -ir 10 -i 1000 -N 100
 else
-    echo "Not generating file data/anharmonic_oscillator_track/N100i1000init0.0m0.2500l-0.0200d10.0000.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_track.py data/anharmonic_oscillator_track/N100i1000init0.0m0.2500l-0.0200d10.0000.csv -i 1 10 20 40 80 100 1000
-$PYTHON src/create_plots_track.py data/anharmonic_oscillator_track/N100i1000init0.0m0.2500l-0.0200d10.0000.csv -i 1 50 100 200 400 1000
+$PYTHON src/create_plots_track.py $FILE -i 1 10 20 40 80 100 1000
+$PYTHON src/create_plots_track.py $FILE -i 1 50 100 200 400 1000
 
 echo
 
-if [ ! -f data/anharmonic_oscillator_track/N1000i1000init0.0m0.2500l-0.0200d10.0000.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/anharmonic_oscillator_track/N1000i1000init0.0m0.2500l-0.0200d10.0000.csv"
+FILE=data/anharmonic_oscillator_track/N1000i1000init0.0m0.2500l-0.0200d10.0000.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/anharmonic_oscillator.py -m 0.25 -init 0 -ir 10 -i 1000 -N 1000
 else
-    echo "Not generating file data/anharmonic_oscillator_track/N1000i1000init0.0m0.2500l-0.0200d10.0000.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_track.py data/anharmonic_oscillator_track/N1000i1000init0.0m0.2500l-0.0200d10.0000.csv -i 1 10 20 40 80 100 1000
+$PYTHON src/create_plots_track.py $FILE -i 1 10 20 40 80 100 1000
 
 echo
 
-if [ ! -f data/anharmonic_oscillator_track/N100i10000init0.0m0.2500l-0.0200d10.0000.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/anharmonic_oscillator_track/N100i10000init0.0m0.2500l-0.0200d10.0000.csv"
+FILE=data/anharmonic_oscillator_track/N100i10000init0.0m0.2500l-0.0200d10.0000.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/anharmonic_oscillator.py -m 0.25 -init 0 -ir 10 -i 10000 -N 100
 else
-    echo "Not generating file data/anharmonic_oscillator_track/N100i10000init0.0m0.2500l-0.0200d10.0000.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_track.py data/anharmonic_oscillator_track/N100i10000init0.0m0.2500l-0.0200d10.0000.csv -i 100 1000 2000 3000 5000 10000
+$PYTHON src/create_plots_track.py $FILE -i 100 1000 2000 3000 5000 10000
 
 echo
 
-if [ ! -f data/anharmonic_oscillator_track/N200i10000init0.0m0.2500l-0.0200d10.0000.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/anharmonic_oscillator_track/N200i10000init0.0m0.2500l-0.0200d10.0000.csv"
+FILE=data/anharmonic_oscillator_track/N200i10000init0.0m0.2500l-0.0200d10.0000.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/anharmonic_oscillator.py -m 0.25 -init 0 -ir 10 -i 10000 -N 200
 else
-    echo "Not generating file data/anharmonic_oscillator_track/N200i10000init0.0m0.2500l-0.0200d10.0000.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_track.py data/anharmonic_oscillator_track/N200i10000init0.0m0.2500l-0.0200d10.0000.csv -i 100 1000 2000 3000 5000 10000
+$PYTHON src/create_plots_track.py $FILE -i 100 1000 2000 3000 5000 10000
 
 echo
 
 # classical limit harmonic oscillator
-if [ ! -f data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-5.00-5.00-0.10-N1000.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-5.00-5.00-0.10-N1000.csv"
+FILE=data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-5.00-5.00-0.10-N1000.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/harmonic_oscillator_classical_limit.py -N 1000
 else
-    echo "Not generating file data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-5.00-5.00-0.10-N1000.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_classical_limit.py data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-5.00-5.00-0.10-N1000.csv
+$PYTHON src/create_plots_classical_limit.py $FILE
 
 echo
 
-
-if [ ! -f data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-3.00-3.00-0.05-N1000.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-3.00-3.00-0.05-N1000.csv"
+FILE=data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-3.00-3.00-0.05-N1000.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/harmonic_oscillator_classical_limit.py -N 1000 -b " -3:3:0.05"
 else
-    echo "Not generating file data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-3.00-3.00-0.05-N1000.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_classical_limit.py data/harmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-3.00-3.00-0.05-N1000.csv
+$PYTHON src/create_plots_classical_limit.py $FILE
 
 echo
 
 # classical limit anharmonic oscillator
-if [ ! -f data/anharmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-10.00-10.00-0.10-N100.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/anharmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-10.00-10.00-0.10-N100.csv"
+FILE=data/anharmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-10.00-10.00-0.10-N100.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/anharmonic_oscillator_classical_limit.py -N 100 -b " -10:10:0.1" -init -5 -ir 2 -i 1000
 else
-    echo "Not generating file data/anharmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-10.00-10.00-0.10-N100.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_classical_limit.py data/anharmonic_oscillator_classical_limit/h0.00-2.00-0.0100_-10.00-10.00-0.10-N100.csv
+$PYTHON src/create_plots_classical_limit.py $FILE
 
 echo
 
 # lambda parameter anharmonic oscillator
-if [ ! -f data/anharmonic_oscillator_lambda_parameter/d0.00-10.00-0.10s-5.00-5.00-0.10-N1000-i100.csv ] || [ "${1}" = "-f" ] || [ "${1}" = "--force" ]; then
-    echo "Generating file data/anharmonic_oscillator_lambda_parameter/d0.00-10.00-0.10s-5.00-5.00-0.10-N1000-i100.csv"
+FILE=data/anharmonic_oscillator_lambda_parameter/d0.00-10.00-0.10s-5.00-5.00-0.10-N1000-i100.csv
+if [ ! -f $FILE ] || [ "$force" = true ]; then
+    echo "Generating file $FILE"
     $PYTHON src/anharmonic_oscillator_lambda_parameter.py -init 0 -ir 10 -i 100 -N 1000
 else
-    echo "Not generating file data/anharmonic_oscillator_lambda_parameter/d0.00-10.00-0.10s-5.00-5.00-0.10-N1000-i100.csv"
+    echo "Not generating file $FILE"
 fi
-$PYTHON src/create_plots_lambda_parameter.py data/anharmonic_oscillator_lambda_parameter/d0.00-10.00-0.10s-5.00-5.00-0.10-N1000-i100.csv
+$PYTHON src/create_plots_lambda_parameter.py $FILE
 
 echo
